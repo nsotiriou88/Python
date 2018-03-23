d2441e35880dc8ea2b8d9df5a247e02d7db23c12
nsotiriou88 <nsotiriou88@gmail.com>
2018-03-23 13:03:25 +0000

    fixed database converting

--- DatabaseConverter.py
+++ DatabaseConverter.py
@@ -2,23 +2,72 @@ import sqlite3
 
 
 # Connect to appropriate Database
-db = sqlite3.connect("TESTING_NICK.sqlite")
+database = "TESTING_NICK"
+db = sqlite3.connect("TESTING_NICK"+".sqlite")
 cursor = db.cursor()
+conn = sqlite3.connect("TESTING_NICK"+"_Converted.sqlite")
+cursor2 = conn.cursor()
 
 # Get all tables and remove bad characters
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-results = cursor.fetchall()
-i = 0
-for table in results:
-    print(results[i])
-    results[i] = table[2:-3]
-    print(results[i])
+dbTables = cursor.fetchall()
 
-    i += 1
+tables = []
+
+for table in dbTables:
+    temp = list(table)
+    tables.append(temp[0])
+
+print('The tables in the Database are:', tables)
+
+for table in tables:
+    conn.execute("CREATE TABLE " + table + " (timestamp REAL, id TEXT, meas TEXT, PRIMARY KEY(timestamp, id, meas))")
+    cursor.execute("SELECT timestamp, id, meas FROM " + table)
+    rows = cursor.fetchall()
+    for row in rows:
+        # print(row[2]) #DEBUG
+        temp = ''
+        i = 1
+        for char in row[2]:
+            if char == '{':
+                temp += "{'anchor': '"
+                i = 1
+            elif char == ' ' and i == 1:
+                temp += "', 'dist': "
+                i += 1
+            elif char == ' ' and i == 2:
+                temp += ", 'tqf': "
+                i += 1
+            elif char == ' ' and i == 3:
+                temp += ", 'toa': "
+                i += 1
+            elif char == ' ' and i == 4:
+                temp += ", 'rssi': "
+                i += 1
+            elif char == ' ' and i == 5:
+                temp += ", 'tqf2': "
+                i += 1
+            elif char == ' ' and i == 6:
+                temp += ", 'rssi2': "
+                i += 1
+            elif char == '}':
+                temp += "},"
+            elif char == ']':
+                temp = temp[:-1]
+                temp += "]"
+            else:
+                temp += char
+
+
+        cursor2.execute("INSERT INTO " + table + " (timestamp, id, meas) VALUES (?, ?, ?)", (row[0], row[1], temp))
+        conn.commit()
+
+        # print(temp) #DEBUG
+
+
+
 
 #DEBUG
-print(results)
-print(type(results), len(results))
-print(results[0])
 
-print(results[0][:-3])
+db.close()
+conn.close()
