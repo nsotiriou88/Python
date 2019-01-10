e163345a1702680d054c99d5dd406b024117b8dc
Nicholas Sotiriou <nsotiriou88@gmail.com>
2019-01-10 12:52:47 +0000

    REST API

---
+++ python_rest/server.py
@@ -0,0 +1,53 @@
+'''Need to install modules:
+$ virtualenv venv
+$ source venv/bin/activate
+$ pip install flask flask-jsonpify flask-sqlalchemy flask-restful
+$ pip freeze
+
+Acess:
+http://127.0.0.1:5002/employees
+http://127.0.0.1:5002/tracks
+http://127.0.0.1:5002/employees/8
+
+Site for rest API example:
+https://www.codementor.io/sagaragarwal94/building-a-basic-restful-api-in-python-58k02xsiq
+'''
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
+# from flask_jsonpify import jsonify  # (Need: pip install flask-jsonpify)
+
+db_connect = create_engine('sqlite:///chinook.db')
+app = Flask(__name__)
+api = Api(app)
+
+class Employees(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select * from employees") # This line performs query and returns json result
+        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+
+class Tracks(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+class Employees_Name(Resource):
+    def get(self, employee_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+        
+
+api.add_resource(Employees, '/employees') # Route_1
+api.add_resource(Tracks, '/tracks') # Route_2
+api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
+
+
+if __name__ == '__main__':
+     app.run(port='5002')
